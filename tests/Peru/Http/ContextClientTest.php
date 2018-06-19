1ae1ea7459eaf74cf3d135b7ee499e7e936de721
Giancarlos Salas <giansalex@gmail.com>
2018-06-18 22:15:52 -0500

    add http test

---
+++ tests/Peru/Http/ContextClientTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giansalex
+ * Date: 18/06/2018
+ * Time: 22:07
+ */
+
+namespace Tests\Peru\Http;
+
+use Peru\Http\ContextClient;
+
+class ContextClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGet()
+    {
+        $client = new ContextClient();
+        $result = $client->get('https://httpbin.org/get?value=1');
+
+        $obj = json_decode($result);
+
+        $this->assertTrue(isset($obj->args->value));
+    }
+
+    public function testPost()
+    {
+        $client = new ContextClient();
+        $result = $client->post('https://httpbin.org/post', [
+            'value' => 1,
+        ]);
+
+        $obj = json_decode($result);
+
+        $this->assertTrue(isset($obj->form->value));
+    }
+}
