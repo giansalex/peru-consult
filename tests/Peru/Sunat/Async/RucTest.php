43e94923f896f89dff32ef78688e161c3a33d55a
Giancarlos Salas <giansalex@gmail.com>
2019-08-13 22:58:46 -0500

    add ruc async test

---
+++ tests/Peru/Sunat/Async/RucTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Peru\Sunat\Async;
+
+use Peru\Http\Async\HttpClient;
+use Peru\Sunat\Async\Ruc;
+use Peru\Sunat\Company;
+use Peru\Sunat\HtmlParser;
+use Peru\Sunat\RucHtmlParser;
+use PHPUnit\Framework\TestCase;
+use React\EventLoop\Factory;
+
+class RucTest extends TestCase
+{
+    public function testGetRuc()
+    {
+        $loop = Factory::create();
+        $cs = new Ruc(new HttpClient($loop), new RucHtmlParser(new HtmlParser()));
+        $promise = $cs->get('10401510465');
+        $promise->then(function (?Company $company) {
+            $this->assertNotNull($company);
+            $this->assertEquals('10401510465', $company->ruc);
+        }, function ($e) {
+            $this->fail($e);
+        });
+
+        $loop->run();
+    }
+}
