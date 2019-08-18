4ad35c8489069243eae3ed2db0e80eb4c9cdcc71
Giancarlos Salas <giansalex@gmail.com>
2019-08-18 14:01:25 -0500

    set strict type on tests

--- tests/Peru/Jne/DniParserTest.php
+++ tests/Peru/Jne/DniParserTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Peru\Jne;
 
 use Peru\Jne\DniParser;
--- tests/Peru/Sunat/ClientStubDecorator.php
+++ tests/Peru/Sunat/ClientStubDecorator.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Peru\Sunat;
 
 use Peru\Http\ClientInterface;
