6a5bc852dbb1f8fec857f317be54233610293b58
Giancarlos Salas <giansalex@gmail.com>
2021-05-18 15:55:46 -0500

    Update random url

--- src/Peru/Sunat/Endpoints.php
+++ src/Peru/Sunat/Endpoints.php
@@ -9,5 +9,5 @@ final class Endpoints
     public const CONSULT = 'https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/jcrS00Alias';
     public const RANDOM = 'https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/captcha?accion=random';
     public const USER_VALIDEZ = 'https://ww3.sunat.gob.pe/cl-ti-itestadousr/usrS00Alias';
-    public const RANDOM_PAGE = 'https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/jcrS00Alias?accion=consPorRazonSoc&razSoc=';
+    public const RANDOM_PAGE = 'https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/jcrS00Alias?accion=consPorRazonSoc&razSoc=BVA%20FOODS';
 }
